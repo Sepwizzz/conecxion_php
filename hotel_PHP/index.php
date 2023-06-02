75e5ba7a53fa981612dc7b064eda2c44f5c8b153
sepwizzz <felipeeeeeeeeeee1212@gmail.com>
2023-06-02 01:42:01 -0500

    hola

---
+++ hotel_PHP/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hotel </title>
+    <link rel="stylesheet" href="http://localhost/andrey/hotel_PHP/build/css/a.css">
+
+
+</head>
+
+<body >
+    
+
+    <h3>Hotel - Administración de clientes</h3>
+    
+<div class = 'acciones'>
+    <a href="admin/clientes/crear.php">Crear cliente</a>
+    <a href="admin/clientes/eliminar.php">eliminar clientes </a>
+    <a href="admin/clientes/actualizar.php">actualizar clientes </a>
+    <a href="admin/clientes/seleccionar.php">ver clientes </a>
+
+
+    </div>
+
+
+
+</body>
